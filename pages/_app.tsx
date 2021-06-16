415aa07c945c71a86d8210555f89eee0fdf304cd
timfuhrmann <tf@becklyn.com>
2021-06-16 20:03:32 +0200

    Add loading indicator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import { AppProps } from "next/app";
 import "../app/scss/app.scss";
 import { Navigation } from "../app/js/molecule/Navigation";
+import { useRouter } from "next/router";
+
+NProgress.configure({ showSpinner: false });
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.events.on("routeChangeStart", () => NProgress.start());
+        router.events.on("routeChangeComplete", () => NProgress.done());
+        router.events.on("routeChangeError", () => NProgress.done());
+    }, []);
+
     return (
         <React.Fragment>
             {!pageProps.hideNav && <Navigation />}
