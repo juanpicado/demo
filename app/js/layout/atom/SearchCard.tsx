8224e0cd17a1cdc24068f0e8b1a3ab17a6d6fec4
timfuhrmann <tf@becklyn.com>
2021-07-15 19:37:34 +0200

    Fix search

--- app/js/layout/atom/SearchCard.tsx
+++ app/js/layout/atom/SearchCard.tsx
@@ -1,24 +1,24 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 import { App } from "../../../types/app";
-import { generateImageUrl, generateItemUrl } from "../../lib/util/url";
+import { generateImageUrl } from "../../lib/util/url";
+import { usePrefetch } from "../../lib/util/prefetch";
 
 export const SearchCard: React.FC<App.Item> = ({ poster, title, id, media_type }) => {
+    const { onClick, onMouseEnter, onMouseLeave } = usePrefetch(id, media_type);
+
     if (!poster) return null;
 
     return (
-        <Link href={generateItemUrl(media_type, title, id)} passHref>
-            <a className="search-card">
-                <div className="search-card-image-wrapper">
-                    <Image
-                        src={generateImageUrl(poster)}
-                        alt={title}
-                        layout="fill"
-                        objectFit="cover"
-                    />
-                </div>
-            </a>
-        </Link>
+        <button
+            type="button"
+            className="search-card"
+            onClick={onClick}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}>
+            <div className="search-card-image-wrapper">
+                <Image src={generateImageUrl(poster)} alt={title} layout="fill" objectFit="cover" />
+            </div>
+        </button>
     );
 };
